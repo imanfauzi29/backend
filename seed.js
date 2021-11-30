4617b0a22bdd21da5580f7e5cc121ba1d3e3c59e
Iman <iman@dmk.co.id>
2021-11-30 09:46:39 +0700

    Add seeder

---
+++ seed.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose")
+const path = require("path")
+const { getIp } = require("./src/utils/utils")
+
+const db = process.env.MONGODB_URI
+    ? process.env.MONGODB_URI
+    : "mongodb://127.0.0.1:27017/cbt_online"
+
+const data = [
+    {
+        _model: "Roles",
+        document: {
+            _id: "619c8cd737bf5cb7771caacb",
+            role_name: "Administrator",
+            active: true
+        }
+    },
+    {
+        _model: "Users",
+        document: {
+            first_name: "Iman",
+            ip_address: getIp(),
+            last_name: "Fauzi",
+            username: "admin",
+            password: "admin123",
+            role: "619c8cd737bf5cb7771caacb",
+            email: "admin@gmail.com",
+            active: true
+        }
+    }
+]
+
+const seed = () => {
+    mongoose.connect(db, { useNewUrlParser: true }, (err) => {
+        if (err) console.log(err.message)
+
+        data.forEach((d) => {
+            const Model = require(path.resolve(`./src/models/${d._model}`))
+            mongoose.connection.db.dropCollection(d._model.toLocaleLowerCase(), (err, result) => {
+                if (err) throw new Error(`failed to drop collection: ${err}`)
+                
+                const newModel = new Model(d.document)
+                newModel.save(err => {
+                    if (err) throw new Error(err.message)
+                    console.log(`${d._model} success seed!`);
+                    mongoose.connection.close()
+                })
+            })
+        })
+
+        return
+    })
+}
+
+seed()
